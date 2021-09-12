b3d72f98cccf0b985ac53fa8523ad5308cb1ca35
Manan Arora <mananaroramail@gmail.com>
2021-09-12 23:36:09 +0530

    Flutter

---
+++ climate_app/lib/services/weather.dart
@@ -0,0 +1,70 @@
+import 'package:climate_app/services/location.dart';
+import 'package:climate_app/services/networking.dart';
+
+const apiKey = "ca0a4e734aca7b656877630c3bdb5d91";
+
+class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    Location loc = new Location();
+    await loc.getCurrentLocation();
+
+    var uri = Uri.https('api.openweathermap.org', '/data/2.5/weather',
+        {'appid': apiKey, 'q': cityName, 'units': 'metric'});
+
+    NetworkHelper networkHelper = NetworkHelper(uri);
+
+    var weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location loc = new Location();
+    await loc.getCurrentLocation();
+
+    var uri = Uri.https('api.openweathermap.org', '/data/2.5/weather', {
+      'appid': apiKey,
+      'lat': loc.latitude.toString(),
+      'lon': loc.longitude.toString(),
+      'units': 'metric'
+    });
+
+    NetworkHelper networkHelper = NetworkHelper(uri);
+
+    var weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
+  String getWeatherIcon(int condition) {
+    if (condition < 300) {
+      return '🌩';
+    } else if (condition < 400) {
+      return '🌧';
+    } else if (condition < 600) {
+      return '☔️';
+    } else if (condition < 700) {
+      return '☃️';
+    } else if (condition < 800) {
+      return '🌫';
+    } else if (condition == 800) {
+      return '☀️';
+    } else if (condition <= 804) {
+      return '☁️';
+    } else {
+      return '🤷‍';
+    }
+  }
+
+  String getMessage(int temp) {
+    if (temp > 25) {
+      return 'It\'s 🍦 time';
+    } else if (temp > 20) {
+      return 'Time for shorts and 👕';
+    } else if (temp < 10) {
+      return 'You\'ll need 🧣 and 🧤';
+    } else {
+      return 'Bring a 🧥 just in case';
+    }
+  }
+}
